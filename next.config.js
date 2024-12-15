cbb5b81ddc1a0d003b2c898f2dbae3f2258483f7
kokistr <125700374+kokistr@users.noreply.github.com>
2024-12-15 22:10:56 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    appDir: true,
-  },
-  // Next.jsのルートディレクトリをsrcに設定
-  basePath: '',
 };
 
 module.exports = nextConfig;
