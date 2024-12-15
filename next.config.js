c2848f0a7fdc838b4ebeef816aee0f0bc6c92744
kokistr <125700374+kokistr@users.noreply.github.com>
2024-12-15 17:49:22 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'fonts/',
-          },
-        },
-      ],
-    });
+  output: 'standalone'}
 
-    return config;
-  },
-};
-
-export default nextConfig;
+module.exports = nextConfig
