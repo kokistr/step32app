7595ac4ac04a1cf4a1184bac9f6d599d1d53fdbc
kokistr <125700374+kokistr@users.noreply.github.com>
2024-12-16 00:49:46 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //output: 'standalone',
-  experimental: {
-    appDir: true, // appルーターを有効化
-  },
-  // srcディレクトリをベースディレクトリとして指定
   distDir: '.next',
 };
 
